595e3fa76dcb0fb348abda4c76a62669dbc6e0ef
emilroman <emir23us@yahoo.com>
2020-03-16 22:17:09 +0200

    Add project files.

---
+++ VotingWeb/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace VotingWeb.Controllers
+{
+    using global::VotingWeb.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Fabric;
+
+    public class HomeController : Controller
+    {
+        private readonly StatelessServiceContext serviceContext;
+
+        public HomeController(StatelessServiceContext context)
+        {
+            this.serviceContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new HomeViewModel()
+            {
+                AppID = Startup.AppId.ToString(),
+                NodeName = serviceContext.NodeContext.NodeName,
+                RequestIP = ""/*Request.HttpContext.Connection.RemoteIpAddress.ToString()*/
+            };
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult Error()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ VotingWeb/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotingWeb.Models
+{
+    public class HomeViewModel
+    {
+        public string AppID { get; set; }
+        public string NodeName { get; set; }
+        public string RequestIP { get; set; }
+    }
+}
